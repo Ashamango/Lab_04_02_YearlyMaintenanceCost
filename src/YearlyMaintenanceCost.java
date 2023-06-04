97e6e159d391e46a942f1b67182ea62414dfb347
Ashamango <ashaanupdas@gmail.com>
2023-06-03 21:17:55 -0400

    Displays the seasonal costs.

---
+++ src/YearlyMaintenanceCost.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class YearlyMaintenanceCost {
+    public static void main(String[] args) {
+        //
+        double springCost = 250.50;
+        double summerCost = 320.50;
+        double winterCost = 275.50;
+        double fallCost = 230.50;
+        double total = 0;
+        total = springCost + summerCost + winterCost + fallCost;
+        System.out.println("The seasonal costs is " + total + " dollars.");
+
+    }
+}
